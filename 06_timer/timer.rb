8760d69b58d812fd754dcb188ed0cd3cc4f4d80b
Ryan H. Jewell <rjewelld@gmail.com>
2017-03-17 16:46:24 -0400

    Finished timer testing

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
+
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize(seconds=0)
+    @seconds=seconds
+  end
+
+  def timerize(time)
+    if time<10
+      time=time.to_s
+      return '0'+time
+    else
+      return time.to_s
+    end
+  end
+  def time_string
+    minutes=timerize((@seconds/60)%60)
+    hours=timerize((@seconds/60)/60)
+    secs=timerize(@seconds%60)
+    return "#{hours}:#{minutes}:#{secs}"
+  end
 end

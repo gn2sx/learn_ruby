2097966eb1ec68f17a1079f51086062d836f090d
Ryan H. Jewell <rjewelld@gmail.com>
2017-03-17 16:20:41 -0400

    Finished book_titles tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_writer :title
+  def initialize(title='')
+    @title=title
+  end
+  def title()
+    little_words=['and','the','a','an','in','of']
+    @title=@title.split(' ')
+    cur_index=0
+    @title.each{|w| w.capitalize! unless cur_index!=0&&little_words.include?(w);cur_index+=1}
+    finalTitle=''
+    @title.each{|w| finalTitle+="#{w} "}
+    @title=finalTitle
+    return @title.strip
+  end
 end

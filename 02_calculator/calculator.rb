b258ec400d06fae4e4802fe2ce97a7672a0ce079
Ryan H. Jewell <rjewelld@gmail.com>
2017-03-17 13:35:09 -0400

    Finished calculator testing

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+# `add` takes two parameters and adds them
+#
+# `subtract` takes two parameters and subtracts the second from the first
+#
+# `sum` takes an *array* of parameters and adds them all together
+def add(num1,num2)
+  num1+num2
+end
+def subtract(num1,num2)
+  num1-num2
+end
+def sum(nums=[])
+  total=0
+  nums.each{|i| total+=i}
+  return total
+end

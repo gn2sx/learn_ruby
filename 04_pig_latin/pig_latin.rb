339e2670e05b7f6b52ba61a956e781e01e546e32
Ryan H. Jewell <rjewelld@gmail.com>
2017-03-17 15:51:38 -0400

    Finished pig_latin test

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
 #write your code here
+def translate(phrase)
+  phrase.downcase!
+  phrase=phrase.split(' ') #split the phrase up so we can work on each word individually
+  vowels=['a','e','i','o','u']
+  singles=['sch','qu']
+  phrase.each do |w|
+    len=w.length
+    len.times do |i|
+      break if vowels.include?(w[0])
+      w[w.length]=w[0]
+      w[0]=''
+    end
+    w << "ay"
+  end
+  finalPhrase=''
+  phrase.each{|w| finalPhrase+="#{w} "}
+  return finalPhrase.strip
+end
